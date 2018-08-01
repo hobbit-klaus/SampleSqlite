382237d037d6ff760fcb117b8d2e10c3f1ff7268
Sung Yu <hwchon1234@naver.com>
2018-08-01 15:30:49 +0900

    Add Stater Code

--- SampleSqlite/SampleSqlite/ViewController.swift
+++ SampleSqlite/SampleSqlite/ViewController.swift
@@ -14,12 +14,63 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func createTable() {
+        print("CREATE TAPPED")
     }
-
+    
+    @IBAction func insertUser() {
+        print("INSERT TAPPED")
+        let alert = UIAlertController(title: "Insert User", message: nil, preferredStyle: .alert)
+        alert.addTextField { (tf) in tf.placeholder = "Name" }
+        alert.addTextField { (tf) in tf.placeholder = "Email" }
+        let action = UIAlertAction(title: "Submit", style: .default) { (_) in
+            guard let name = alert.textFields?.first?.text,
+                let email = alert.textFields?.last?.text
+                else { return }
+            print(name)
+            print(email)
+            
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func listUsers() {
+        print("LIST TAPPED")
+        
+    }
+    
+    @IBAction func updateUser() {
+        print("UPDATE TAPPED")
+        let alert = UIAlertController(title: "Update User", message: nil, preferredStyle: .alert)
+        alert.addTextField { (tf) in tf.placeholder = "User ID" }
+        alert.addTextField { (tf) in tf.placeholder = "Email" }
+        let action = UIAlertAction(title: "Submit", style: .default) { (_) in
+            guard let userIdString = alert.textFields?.first?.text,
+                let email = alert.textFields?.last?.text
+                else { return }
+            print(userIdString)
+            print(email)
+            
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func deleteUser() {
+        print("DELETE TAPPED")
+        let alert = UIAlertController(title: "Update User", message: nil, preferredStyle: .alert)
+        alert.addTextField { (tf) in tf.placeholder = "User ID" }
+        let action = UIAlertAction(title: "Submit", style: .default) { (_) in
+            guard let userIdString = alert.textFields?.first?.text else { return }
+            print(userIdString)
+            
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
